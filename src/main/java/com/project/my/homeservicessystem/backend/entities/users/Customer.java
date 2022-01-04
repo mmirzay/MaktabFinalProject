319e4e40632ed785471087d76e53c32f90423e7f
mmirzay <mirzay.mohsen@gmail.com>
2022-01-05 02:16:57 +0330

    added constructor and builder methods to customer entity.

--- src/main/java/com/project/my/homeservicessystem/backend/entities/users/Customer.java
+++ src/main/java/com/project/my/homeservicessystem/backend/entities/users/Customer.java
@@ -8,5 +8,21 @@ import lombok.Setter;
 import javax.persistence.Entity;
 
 @Entity
+@NoArgsConstructor
 public class Customer extends User {
+    private Customer(String email, String password, Role role) {
+        this(email, password, role, "", "");
+    }
+
+    private Customer(String email, String password, Role role, String firstName, String lastName) {
+        super(email, password, role, firstName, lastName);
+    }
+
+    public static Customer of(String email, String password, Role role) {
+        return new Customer(email, password, role);
+    }
+
+    public static Customer of(String email, String password, Role role, String firstName, String lastName) {
+        return new Customer(email, password, role, firstName, lastName);
+    }
 }
--- src/main/java/com/project/my/homeservicessystem/backend/entities/users/User.java
+++ src/main/java/com/project/my/homeservicessystem/backend/entities/users/User.java
@@ -1,16 +1,20 @@
 package com.project.my.homeservicessystem.backend.entities.users;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@Builder
 @MappedSuperclass
 public class User {
     @Id
@@ -21,9 +25,14 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
     private String password;
-    private UserStatus status = UserStatus.NEW;
-    private Date registerDate = new Date();
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+    private Date registerDate;
     @ManyToMany
     private Set<Role> roles;
     private double credit;
+
+    public User(String email, String password, Role role, String firstName, String lastName) {
+        this(null, firstName, lastName, email, password, UserStatus.NEW, new Date(), new HashSet(List.of(role)), 0);
+    }
 }
