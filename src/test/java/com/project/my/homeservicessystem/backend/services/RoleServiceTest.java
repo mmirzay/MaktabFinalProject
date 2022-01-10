ef8d42143ade60914385ef38b5cf4405aa9e33f2
mmirzay <mirzay.mohsen@gmail.com>
2022-01-10 16:10:08 +0330

    added TestMethodOrder(MethodOrderer.OrderAnnotation.class) to reorder tests in RoleServiceTest class.

--- src/test/java/com/project/my/homeservicessystem/backend/services/RoleServiceTest.java
+++ src/test/java/com/project/my/homeservicessystem/backend/services/RoleServiceTest.java
@@ -1,8 +1,10 @@
 package com.project.my.homeservicessystem.backend.services;
 
 import com.project.my.homeservicessystem.backend.entities.users.Role;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,6 +13,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class RoleServiceTest {
     @Autowired
     RoleService service;
