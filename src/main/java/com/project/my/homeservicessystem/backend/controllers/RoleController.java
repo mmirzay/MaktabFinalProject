8ef0a1dc5c077da9b2dccbb0070b6a5d62458246
mmirzay <mirzay.mohsen@gmail.com>
2022-01-21 02:46:18 +0330

    added exception handler to role controller while duplicates role created.

--- src/main/java/com/project/my/homeservicessystem/backend/controllers/RoleController.java
+++ src/main/java/com/project/my/homeservicessystem/backend/controllers/RoleController.java
@@ -3,6 +3,7 @@ package com.project.my.homeservicessystem.backend.controllers;
 import com.project.my.homeservicessystem.backend.api.HomeServiceInterface;
 import com.project.my.homeservicessystem.backend.api.dto.in.RoleCreateParam;
 import com.project.my.homeservicessystem.backend.api.dto.out.RoleCreateResult;
+import com.project.my.homeservicessystem.backend.exceptions.ManagerException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,8 +21,13 @@ public class RoleController {
 
     @PostMapping
     public ResponseEntity<RoleCreateResult> create(@RequestBody RoleCreateParam createParam) {
-        RoleCreateResult result = manager.addRole(createParam);
-        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+        try {
+            RoleCreateResult result = manager.addRole(createParam);
+            return ResponseEntity.status(HttpStatus.CREATED).body(result);
+        } catch (ManagerException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, e.getLocalizedMessage());
+        }
     }
 
 }
