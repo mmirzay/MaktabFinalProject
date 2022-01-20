21fef05ac61cbe2ee341908e14fa0b6e51fa9d6e
mmirzay <mirzay.mohsen@gmail.com>
2022-01-21 02:43:02 +0330

    added new class for main services of system using facet design pattern.

---
+++ src/main/java/com/project/my/homeservicessystem/backend/api/HomeServiceInterface.java
@@ -0,0 +1,5 @@
+package com.project.my.homeservicessystem.backend.api;
+
+public interface HomeServiceInterface {
+
+}
---
+++ src/main/java/com/project/my/homeservicessystem/backend/manager/HomeServiceManager.java
@@ -0,0 +1,10 @@
+package com.project.my.homeservicessystem.backend.manager;
+
+import com.project.my.homeservicessystem.backend.api.HomeServiceInterface;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class HomeServiceManager implements HomeServiceInterface {
+}
