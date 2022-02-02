a563904ff2eb9691e29b623afe5040eece358b8b
mmirzay <mirzay.mohsen@gmail.com>
2022-02-02 22:20:08 +0330

    added equals and hashcode to Service and ServiceCategaroy entities.

--- src/main/java/com/project/my/homeservicessystem/backend/entities/services/Service.java
+++ src/main/java/com/project/my/homeservicessystem/backend/entities/services/Service.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @NoArgsConstructor
@@ -37,5 +38,18 @@ public class Service {
 
     public static Service of(String title, double basePrice, String description, ServiceCategory category) {
         return new Service(title, basePrice, description, category);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Service service = (Service) o;
+        return title.equals(service.title) && category.equals(service.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, category);
     }
 }
--- src/main/java/com/project/my/homeservicessystem/backend/entities/services/ServiceCategory.java
+++ src/main/java/com/project/my/homeservicessystem/backend/entities/services/ServiceCategory.java
@@ -1,15 +1,15 @@
 package com.project.my.homeservicessystem.backend.entities.services;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Setter
+@Getter
 public class ServiceCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,5 +19,18 @@ public class ServiceCategory {
 
     public ServiceCategory(String name) {
         this(null, name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ServiceCategory category = (ServiceCategory) o;
+        return name.equals(category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
