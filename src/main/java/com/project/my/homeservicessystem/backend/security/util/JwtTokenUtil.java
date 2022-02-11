bc583e87b75f710d5d42e06d89c1cc4f6a115a69
mmirzay <mirzay.mohsen@gmail.com>
2022-02-11 04:17:07 +0330

    added security config, filter and JWT utility.

---
+++ src/main/java/com/project/my/homeservicessystem/backend/security/util/JwtTokenUtil.java
@@ -0,0 +1,86 @@
+package com.project.my.homeservicessystem.backend.security.util;
+
+import com.project.my.homeservicessystem.backend.entities.users.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+import static java.lang.String.format;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JwtTokenUtil {
+
+    private final String jwtSecret = "secureMustBeSecureNothingElseMatter";
+    private final String jwtIssuer = "example.io";
+    private final long expireValue = System.currentTimeMillis() + 7 * 24 * 60 * 60 * 1000; // 1 week
+
+    public String generateAccessToken(User user) {
+        return Jwts.builder()
+                .setSubject(format("%s,%s", user.getId(), user.getUsername()))
+                .setIssuer(jwtIssuer)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(expireValue))
+                .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                .compact();
+    }
+
+    public String getUserId(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject().split(",")[0];
+    }
+
+    public String getUsername(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject().split(",")[1];
+    }
+
+    public Date getExpirationDate(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getExpiration();
+    }
+
+    public boolean validate(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes()))
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            log.error("Invalid JWT signature - {}", ex.getMessage());
+        } catch (MalformedJwtException ex) {
+            log.error("Invalid JWT token - {}", ex.getMessage());
+        } catch (ExpiredJwtException ex) {
+            log.error("Expired JWT token - {}", ex.getMessage());
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token - {}", ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty - {}", ex.getMessage());
+        }
+        return false;
+
+    }
+
+}
