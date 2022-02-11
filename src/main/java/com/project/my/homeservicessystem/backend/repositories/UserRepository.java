314ff081da77c42c0ad4d9178501ca5108885960
mmirzay <mirzay.mohsen@gmail.com>
2022-02-11 04:16:34 +0330

    added new repository for managing users due to security.

--- src/main/java/com/project/my/homeservicessystem/backend/repositories/UserRepository.java
+++ src/main/java/com/project/my/homeservicessystem/backend/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.project.my.homeservicessystem.backend.repositories;
 import com.project.my.homeservicessystem.backend.entities.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
